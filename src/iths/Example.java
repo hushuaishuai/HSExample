cc73d785cea8064b450415c435ab6ff976848cf0
hushuai <hushuai@sinoiov.com>
2019-05-05 15:03:54 +0800

    Initial commit

---
+++ src/iths/Example.java
@@ -0,0 +1,12 @@
+package iths;
+
+/**
+ * @author hushuai
+ * @date 2019/5/5 - 11:00
+ */
+public class Example {
+    public static void main(String[] args) {
+        /*ArrayList*/
+
+    }
+}
---
+++ src/iths/com.java
@@ -0,0 +1,10 @@
+package iths;
+
+import java.util.ArrayList;
+//单行注释
+public class com {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        ArrayList arr = new ArrayList();
+    }
+}
